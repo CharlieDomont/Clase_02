dc4a4ce75f79ff2b010c7f60d1e8809250ef0c66
Carlos <cado18@hotmail.com>
2019-05-23 20:00:07 -0500

    Clase #2

---
+++ src/clase_02.java
@@ -0,0 +1,14 @@
+public class clase_02 {
+    int num;
+    char letra;
+    double real;
+    String hola;
+
+
+    public void imprimir() {
+
+        System.out.println("Clase #2");
+        System.out.println("Mi nombre -> " +nombre);
+    }
+     int resultado(int edad)
+}
